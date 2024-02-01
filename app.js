b734c83a245065fce427fd2bf429ba10c71a1700
sunilpatil2001 <psunils9147@gmail.com>
2024-02-01 17:46:04 +0530

    header added

--- app.js
+++ app.js
@@ -7,6 +7,26 @@ const path = require('path');
 app.use(cors())
 app.use(express.json());
 app.use(express.static(path.join(__dirname + "/public")))
+app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://bucolic-pony-cc177a.netlify.app"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+  
+    next();
+  });
 
 const con = mysql.createConnection({
     host: 'sql6.freesqldatabase.com',
