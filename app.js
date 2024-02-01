9ccbece2333997b6e80494d7b7ca901b8ca3244c
sunilpatil2001 <psunils9147@gmail.com>
2024-02-01 17:48:46 +0530

    header added

--- app.js
+++ app.js
@@ -42,14 +42,6 @@ con.connect((err) => {
         console.log("connected with database");
 });
 
-app.get("/", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
-})
-
 app.post("/login", (req, res) => {
     const q = "SELECT admin FROM Directors WHERE email= ? and pass = ?";
     con.query(q, [req.body.email, req.body.password], (err, result) => {
