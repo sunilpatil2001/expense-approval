7db1f39a315377dff68827143bb920eada9c8b3a
sunilpatil2001 <psunils9147@gmail.com>
2024-02-01 18:03:13 +0530

    header added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const mysql = require('mysql2');
 const path = require('path');
 const corsOptions = {
-  origin:"http://localhost/3000",
+  origin:"http://localhost:3000",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",};
 app.use(cors(corsOptions));
 app.use(express.json());
